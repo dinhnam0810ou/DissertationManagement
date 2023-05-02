cb8800afbba07bf91d6972bba4f2e39ff9f302ad
xuanthang77 <0915454715t@gmail.com>
2023-05-03 00:08:00 +0700

    update

--- ck-app/src/components/Newdissertation.js
+++ ck-app/src/components/Newdissertation.js
@@ -1,78 +1,104 @@
-import React, { useState } from 'react';
+import { Button, MenuItem, TextField } from "@mui/material";
+import React, { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 
 function Newdissertation() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm();
+  const handleAddDessertation = (data) => {
+    console.log(data);
+  };
   return (
     <>
-      <div class="container-fluid pt-4 px-4">
-        <div class="bg-light text-center rounded p-4">
-          <div class="d-flex align-items-center justify-content-between mb-4">
-            <h6 class="mb-0">Statistics</h6>
-            <a href="">Show All</a>
-          </div>
-          <div class="table-responsive">
-            <table class="table text-start align-middle table-bordered table-hover mb-0">
-              <thead>
-                <tr class="text-dark">
-                  <th scope="col"><input class="form-check-input" type="checkbox"/></th>
-                  <th scope="col">Date</th>
-                  <th scope="col">Invoice</th>
-                  <th scope="col">Customer</th>
-                  <th scope="col">Amount</th>
-                  <th scope="col">Status</th>
-                  <th scope="col">Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td><input class="form-check-input" type="checkbox"/></td>
-                  <td>01 Jan 2045</td>
-                  <td>INV-0123</td>
-                  <td>Jhon Doe</td>
-                  <td>$123</td>
-                  <td>Paid</td>
-                  <td><a class="btn btn-sm btn-primary" href="">Detail</a></td>
-                </tr>
-                <tr>
-                  <td><input class="form-check-input" type="checkbox" /></td>
-                  <td>01 Jan 2045</td>
-                  <td>INV-0123</td>
-                  <td>Jhon Doe</td>
-                  <td>$123</td>
-                  <td>Paid</td>
-                  <td><a class="btn btn-sm btn-primary" href="">Detail</a></td>
-                </tr>
-                <tr>
-                  <td><input class="form-check-input" type="checkbox" /></td>
-                  <td>01 Jan 2045</td>
-                  <td>INV-0123</td>
-                  <td>Jhon Doe</td>
-                  <td>$123</td>
-                  <td>Paid</td>
-                  <td><a class="btn btn-sm btn-primary" href="">Detail</a></td>
-                </tr>
-                <tr>
-                  <td><input class="form-check-input" type="checkbox" /></td>
-                  <td>01 Jan 2045</td>
-                  <td>INV-0123</td>
-                  <td>Jhon Doe</td>
-                  <td>$123</td>
-                  <td>Paid</td>
-                  <td><a class="btn btn-sm btn-primary" href="">Detail</a></td>
-                </tr>
-                <tr>
-                  <td><input class="form-check-input" type="checkbox"/></td>
-                  <td>01 Jan 2045</td>
-                  <td>INV-0123</td>
-                  <td>Jhon Doe</td>
-                  <td>$123</td>
-                  <td>Paid</td>
-                  <td><a class="btn btn-sm btn-primary" href="">Detail</a></td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+      <Controller
+        name="name"
+        control={control}
+        rules={{ required: true }}
+        render={({ field }) => (
+          <TextField
+            {...field}
+            margin="normal"
+            variant="outlined"
+            label="Nhập tên"
+            error={errors.name}
+            fullWidth
+            InputLabelProps={{ shrink: true }}
+          />
+        )}
+      />
+      <Controller
+        name="SVname"
+        control={control}
+        rules={{ required: true }}
+        render={({ field }) => (
+          <TextField
+            {...field}
+            margin="normal"
+            variant="outlined"
+            label="Nhập tên SV1"
+            error={errors.SVname}
+            fullWidth
+            InputLabelProps={{ shrink: true }}
+          />
+        )}
+      />
+      <Controller
+        name="SVname2"
+        control={control}
+        rules={{ required: true }}
+        render={({ field }) => (
+          <TextField
+            {...field}
+            margin="normal"
+            variant="outlined"
+            label="Nhập tên SV2"
+            error={errors.SVname2}
+            fullWidth
+            InputLabelProps={{ shrink: true }}
+          />
+        )}
+      />
+      <TextField
+        fullWidth
+        select
+        margin="normal"
+        variant="outlined"
+        label="Giang vien"
+      >
+        <MenuItem value={"true"}>Đề tham khảo</MenuItem>
+        <MenuItem value={"false"}>Đề private</MenuItem>
+      </TextField>
+      <TextField
+        fullWidth
+        select
+        margin="normal"
+        variant="outlined"
+        label="Giang vien"
+      >
+        <MenuItem value={"true"}>Đề tham khảo</MenuItem>
+        <MenuItem value={"false"}>Đề private</MenuItem>
+      </TextField>
+
+      <Button onClick={handleSubmit(handleAddDessertation)}>Thêm bộ đề </Button>
+      {/* <form>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
         </div>
-      </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" />
+        </div>
+        <div class="mb-3 form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form> */}
     </>
   );
 }
